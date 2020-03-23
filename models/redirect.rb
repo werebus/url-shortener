b20c9ad17716ffd9edcce7762dadea79b1d27ac6
Matt Moretti <werebus@gmail.com>
2020-03-23 15:19:40 -0400

    Auto-slugger

--- models/redirect.rb
+++ models/redirect.rb
@@ -3,4 +3,19 @@
 class Redirect < ActiveRecord::Base
   validates :slug, :url, presence: true
   validates :slug, format: { with: /\A[a-z]+\z/ }
+
+  before_validation :assign_next_slug, on: :create, if: ->{ slug.blank? }
+
+  private
+
+  def assign_next_slug
+    new_slug = Redirect.order(:slug).last.slug.succ
+    loop do
+      unless Redirect.exists?(slug: new_slug)
+        self.slug = new_slug
+        break
+      end
+      new_slug = new_slug.succ
+    end
+  end
 end
